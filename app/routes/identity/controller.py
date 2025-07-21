99f8e0dad0fe6803fe831a69134d94a5558c9899
Hoang Anh <hoanganhlhbk@gmail.com>
2025-07-21 14:39:28 +0700

    auth request handle non json response

--- app/routes/identity/controller.py
+++ app/routes/identity/controller.py
@@ -2,6 +2,7 @@ from .schemas import AuthRequest
 from fastapi import Response
 import aiohttp
 from app.config import settings
+import json
 
 class AuthController:
     @staticmethod
@@ -9,7 +10,12 @@ class AuthController:
         # call openstack authentication server
         async with aiohttp.ClientSession() as session:
             async with session.post(f"{settings.openstack_config.get('endpoints', '').get('identity', '')}auth/tokens", json={"auth": auth_request.model_dump(exclude_none=True)}) as resp:
-                response_data = await resp.json()
+                try:
+                    response_data = await resp.json()
+                except aiohttp.ContentTypeError:
+                    response_data = await resp.text()
+                except json.decoder.JSONDecodeError:
+                    response_data = await resp.text()
                 if resp.status != 201:
                     response.status_code = resp.status
                     return response_data
